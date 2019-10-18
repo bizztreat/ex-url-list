55ce2ea389afcb15e6a2aa7798c65ba477f6288d
Tomáš Votava <tomas.votava@bizztreat.com>
2019-10-18 14:30:19 +0200

    Only use basename from URL

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def ungzip(b):
 
 
 ## Read config file
-with open("/data/config.json" if os.path.exists("/data/config.json") else "conf.json","r") as conf_file:
+with open("/data/config.json" if os.path.exists("/data/config.json") else "conf.json", "r") as conf_file:
     conf = json.load(conf_file)["parameters"]
 
 if conf["debug"]:
@@ -55,18 +55,18 @@ if conf["debug"]:
 ## Print debug info
 if conf["incremental"]:
     for mapping in conf["mapping"]:
-        output_date = (datetime.today() + timedelta(days=int(mapping["date-condition"]))).replace(hour=0,minute=0,second=0,microsecond=0)
-        if mapping["date-comparison"]!="BETWEEN":
-            print("Will download only files with date {0}{1} conforming expression {2}".format(mapping["date-comparison"],output_date,mapping["matching"]))
+        output_date = (datetime.today() + timedelta(days=int(mapping["date-condition"]))).replace(hour=0, minute=0, second=0, microsecond=0)
+        if mapping["date-comparison"] != "BETWEEN":
+            print("Will download only files with date {0}{1} conforming expression {2}".format(mapping["date-comparison"], output_date, mapping["matching"]))
         else:
-            upper_date = (datetime.today() + timedelta(days=int(mapping["date-condition-upper"]))).replace(hour=0,minute=0,second=0,microsecond=0)
-            print("Will download only files with date BETWEEN {0} AND {1} conforming expression {2}".format(output_date,upper_date,mapping["matching"]))
+            upper_date = (datetime.today() + timedelta(days=int(mapping["date-condition-upper"]))).replace(hour=0, minute=0, second=0, microsecond=0)
+            print("Will download only files with date BETWEEN {0} AND {1} conforming expression {2}".format(output_date, upper_date, mapping["matching"]))
 
 
 ## Get mapping from configuration matching the link
 def get_output_mapping(link):
     for mapping in conf["mapping"]:
-        if re.match(mapping["matching"],link):
+        if re.match(mapping["matching"], link):
             return mapping
     return None
 
@@ -77,7 +77,7 @@ def get_mapping_date(link):
         return None
     else:
         try:
-            date = re.search(m["date-search"],link).group(1)
+            date = re.search(m["date-search"], link).group(1)
             return dateparse(date)
         except Exception as e:
             print(e)
@@ -97,17 +97,17 @@ def get_date_conforms(link):
     m = get_output_mapping(link)
     d = get_mapping_date(link)
     if m==None or d==None: return None
-    comp_date = (datetime.today() + timedelta(days=int(m["date-condition"]))).replace(hour=0,minute=0,second=0,microsecond=0)
-    if m["date-comparison"]=="<":
+    comp_date = (datetime.today() + timedelta(days=int(m["date-condition"]))).replace(hour=0, minute=0, second=0, microsecond=0)
+    if m["date-comparison"] == "<":
         return d < comp_date
-    elif m["date-comparison"]==">":
+    elif m["date-comparison"] == ">":
         return d > comp_date
-    elif m["date-comparison"]=="=":
+    elif m["date-comparison"] == "=":
         return d == comp_date
-    elif m["date-comparison"]=="!=":
+    elif m["date-comparison"] == "!=":
         return d != comp_date
-    elif m["date-comparison"]=="BETWEEN":
-        upper_date = (datetime.today() + timedelta(days=int(m["date-condition-upper"]))).replace(hour=0,minute=0,second=0,microsecond=0)
+    elif m["date-comparison"] == "BETWEEN":
+        upper_date = (datetime.today() + timedelta(days=int(m["date-condition-upper"]))).replace(hour=0, minute=0, second=0, microsecond=0)
         return (d >= comp_date and d <= upper_date)
     else:
         return False
@@ -115,8 +115,8 @@ def get_date_conforms(link):
 
 ## Basic processors for different file types
 ## CSV processor simply appends to an existing csv file or creates a new one should it not already exist
-def process_csv(fname,output,content,link):
-    output = os.path.join(OUTPUT_DIR,output)
+def process_csv(fname, output, content, link):
+    output = os.path.join(OUTPUT_DIR, output)
     if os.path.exists(output):
         appending = True
     else:
@@ -125,28 +125,28 @@ def process_csv(fname,output,content,link):
     with StringIO(content.decode("utf8")) as infile:
         csv_in = csv.reader(infile)
         ## Append or create new file and write
-        with open(output,"a" if appending else "w",newline='\n',encoding="utf-8") as outfile:
+        with open(output, "a" if appending else "w", newline='\n', encoding="utf-8") as outfile:
             csv_out = csv.writer(outfile)
             rownum = 0
             for row in csv_in:
                 rownum+=1
-                if rownum==1:
+                if rownum == 1:
                     if not output in PROCESSED_TABLES:
                         PROCESSED_TABLES[output] = len(row)
-                if appending and rownum==1:
+                if appending and rownum == 1:
                     ## Skip header
                     continue
                 if len(row)>PROCESSED_TABLES[output]:
-                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output],len(row)))
+                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output], len(row)))
                     print("Omitting last columns")
                     row = row[:PROCESSED_TABLES[output]]
                 elif len(row)<PROCESSED_TABLES[output]:
-                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output],len(row)))
+                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output], len(row)))
                     print("Filling with NULLs")
                     actual_columns = len(row)
                     for appended_row_id in range(PROCESSED_TABLES[output]-actual_columns):
                         row.append("")
-                if rownum==1:
+                if rownum == 1:
                     ## Add columns to header
                     if conf["generate-pk"]:
                         row.append(conf["primary-key"])
@@ -155,50 +155,50 @@ def process_csv(fname,output,content,link):
                 else:
                     ## Add appended columns data
                     if conf["generate-pk"]:
-                        pk = "{0}-{1}".format(link,rownum-1)
+                        pk = "{0}-{1}".format(os.path.basename(link), rownum-1)
                         if conf["hash-pk"]:
                             pk = md5(pk.encode("utf8")).hexdigest().upper()
                         row.append(pk)
                     if conf["add-filename"]:
-                            row.append(link)
+                        row.append(os.path.basename(link))
                 ## Encode row to 'utf-8'
                 #row = list(map(str.encode,row))
                 csv_out.writerow(row)
         if conf["debug"]: print("Written {0} rows".format(rownum))
 
 ## TXT processor checks whether the format conforms with TSV, converts to CSV and then processes it via CSV processor
-def process_txt(fname,output,content,link):
-    output = os.path.join(OUTPUT_DIR,output)
+def process_txt(fname, output, content, link):
+    output = os.path.join(OUTPUT_DIR, output)
     if os.path.exists(output):
         appending = True
     else:
         appending = False
     if conf["debug"]: print("Processing TXT file {0}".format(output))
     with StringIO(content.decode("utf8")) as infile:
-        csv_in = csv.reader(infile,dialect="excel-tab")
+        csv_in = csv.reader(infile, dialect="excel-tab")
         ## Append or create new file and write
-        with open(output,"a" if appending else "w",newline='\n',encoding="utf-8") as outfile:
-            csv_out = csv.writer(outfile,dialect="excel")
+        with open(output, "a" if appending else "w", newline='\n', encoding="utf-8") as outfile:
+            csv_out = csv.writer(outfile, dialect="excel")
             rownum = 0
             for row in csv_in:
                 rownum+=1
-                if rownum==1:
+                if rownum == 1:
                     if not output in PROCESSED_TABLES:
                         PROCESSED_TABLES[output] = len(row)
                 if len(row)>PROCESSED_TABLES[output]:
-                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output],len(row)))
+                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output], len(row)))
                     print("Omitting last columns")
                     row = row[:PROCESSED_TABLES[output]]
                 elif len(row)<PROCESSED_TABLES[output]:
-                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output],len(row)))
+                    print("Number of columns missmatch, expected {0}, got {1}".format(PROCESSED_TABLES[output], len(row)))
                     print("Filling with NULLs")
                     actual_columns = len(row)
                     for appended_row_id in range(PROCESSED_TABLES[output]-actual_columns):
                         row.append("")
-                if appending and rownum==1:
+                if appending and rownum == 1:
                     ## Skip header
                     continue
-                if rownum==1:
+                if rownum == 1:
                     ## Add columns to header
                     if conf["generate-pk"]:
                         row.append(conf["primary-key"])
@@ -207,12 +207,12 @@ def process_txt(fname,output,content,link):
                 else:
                     ## Add appended columns data
                     if conf["generate-pk"]:
-                        pk = "{0}-{1}".format(link,rownum-1)
+                        pk = "{0}-{1}".format(os.path.basename(link), rownum-1)
                         if conf["hash-pk"]:
                             pk = md5(pk.encode("utf8")).hexdigest().upper()
                         row.append(pk)
                     if conf["add-filename"]:
-                            row.append(link)
+                        row.append(os.path.basename(link))
                 ## Encode row to 'utf-8'
                 #row = list(map(str.encode,row))
                 #print("Number of columns: {0}".format(len(row)))
@@ -235,7 +235,7 @@ def get_link_list(session):
 
     url = conf["endpoint"]["url"]
 
-    resp = session.get(url,params=params)
+    resp = session.get(url, params=params)
 
     ## Check for response status and exit if non-200
     if resp.status_code != 200:
@@ -246,7 +246,7 @@ def get_link_list(session):
     ## Find all links based on re specified in config
     ## The links look basically like:
     ## <url>LINK</url>
-    links = re.findall(conf["re-match"],resp.text)
+    links = re.findall(conf["re-match"], resp.text)
 
     ## But they might be different some time
     if len(links)==0:
@@ -271,22 +271,22 @@ def get_link_list(session):
         else:
             if conf["incremental"]:
                 if get_date_conforms(link):
-                    final_links.append((link,url,urlp))
+                    final_links.append((link, url, urlp))
                 else:
                     #if conf["debug"]: print("File \'{0}\' does not meet increment condition and will be skipped.".format(url))
                     pass
             else:
-                final_links.append((link,url,urlp))
+                final_links.append((link, url, urlp))
     return final_links
 
 
-def process_all_links(session,final_links):
+def process_all_links(session, final_links):
     global LINKS_PROCESSED
     print("Will process {0} links".format(len(final_links)))
     start_time = time()
     completed = True
     ## Download every link from the list
-    for link,url,urlp in final_links:
+    for link, url, urlp in final_links:
         if time()-start_time > TIME_LIMIT:
             completed = False
             print("Timeout of {0} seconds reached, starting over.".format(TIME_LIMIT))
@@ -295,7 +295,7 @@ def process_all_links(session,final_links):
         if url in LINKS_PROCESSED: continue
         LINKS_PROCESSED.append(url)
         ## Check whether gzipped and supported
-        if os.path.splitext(urlp.path)[1].lower()==".gz":
+        if os.path.splitext(urlp.path)[1].lower() == ".gz":
             compressed = True
             ftype = os.path.splitext(urlp.path[:-3])[1]
             basename = os.path.split(urlp.path[:-3])[1]
@@ -305,7 +305,7 @@ def process_all_links(session,final_links):
             basename = os.path.split(urlp.path)[1]
 
         if ftype not in typelist.keys():
-            print("\'{0}\' is an unsupported type and cannot be processed. Path: \'{1}\'".format(ftype,urlp.path))
+            print("\'{0}\' is an unsupported type and cannot be processed. Path: \'{1}\'".format(ftype, urlp.path))
             if conf["abort-on-error"]:
                 print("This is fatal. If you prefer not to exit on first error, set \'abort-on-error\' option to \'false\'.")
                 sys.exit(1)
@@ -334,12 +334,12 @@ def process_all_links(session,final_links):
                     raise Exception("Too many errors")
                 print("Retrying in 5 seconds...")
                 sleep(5)
-                attempt_no+=1
+                attempt_no += 1
                 continue
 
         ## Check response for non-ok state
         if resp.status_code != 200:
-            print("Link: \'{0}\' returned status code {1}.".format(link,resp.status_code))
+            print("Link: \'{0}\' returned status code {1}.".format(link, resp.status_code))
             if conf["abort-on-error"]:
                 print("This is fatal. If you prefer not to exit on first error, set \'abort-on-error\' option to \'false\'.")
                 sys.exit(1)
@@ -350,7 +350,7 @@ def process_all_links(session,final_links):
         else:
             data  = resp.text
         
-        typelist[ftype](basename,outname,data,url)
+        typelist[ftype](basename, outname, data,url)
     
     return completed
 
@@ -361,7 +361,7 @@ while True:
     ## Get links without already processed ones
     final_links = get_link_list(session)
     ## Process all new links, returns true if all was done, False on time limit reach
-    if process_all_links(session,final_links):
+    if process_all_links(session, final_links):
         break
     else:
         continue
